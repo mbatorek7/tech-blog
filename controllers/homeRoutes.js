a41772400c79e7499928b71a58228213b3b28ae3
mbatorek7 <maegan.batorek@valpo.edu>
2023-04-06 22:46:50 -0500

    updated home and dashboard, added some routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,16 +2,26 @@ const Post = require('../models/post');
 const router = require('express').Router();
 
 router.get('/', async(req, res) => {
+    res.render("home")
+});
+
+router.get('/dashboard', async(req, res) => {
     const postData = await Post.findAll();
     const readablePost = postData.map((post) => {
         return post.get({plain: true})
     })
     console.log(readablePost)
-    res.render("home", {readablePost})
+    res.render("dashboard", {readablePost})
 });
 
 router.get('/login', async(req, res) => {
     res.render("login")
 });
+
+router.get('/signup', async(req, res) => {
+    res.render("signup")
+});
+
+
 
 module.exports = router;
